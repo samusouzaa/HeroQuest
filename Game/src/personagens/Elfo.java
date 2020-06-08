47ebe7b6b3229ec422d717a42822323ae7d90735
lenin <lenin@lenin-IdeaPad-L340-15IRH-Gaming>
2020-06-08 01:17:20 -0300

    Começo da criação da classe Elfo

---
+++ Game/src/personagens/Elfo.java
@@ -0,0 +1,25 @@
+package personagens;
+
+public class Elfo {
+	private static final int ATAQUE = 2;
+	private static final int DEFESA = 2;
+	private static final int HP = 6;
+	private static final int IP = 4;
+
+}
+
+//Barbaro. Ataque: 3 dados, defesa: 2 dados, pontos de vida: 8, pontos  ́
+//de inteligencia: 2. Comec ̧a o jogo com uma espada longa, que for- ˆ
+//nece +3 dados para ataque, mas necessita o uso de ambas as maos.  ̃
+//• Anao. Ataque: 2 dados, defesa: 2 dados, pontos de vida: 7, pontos  ̃
+//de inteligencia: 3. Comec ̧a o jogo com uma espada curta, que fornece ˆ
+//+2 dados para ataque.
+//• Elfo. Ataque: 2 dados, defesa: 2 dados, pontos de vida: 6, pontos de
+//inteligencia: 4. Comec ̧a o jogo com uma espada curta, que fornece ˆ
+//+2 dados para ataque. Comec ̧a o jogo com uma magia simple heal.
+//
+//• Feiticeiro. Ataque: 1 dado, defesa: 2 dados, pontos de vida: 4, pon-
+//tos de inteligencia: 6. Comec ̧a o jogo com 3 punhais, cada um forne- ˆ
+//
+//cendo +1 dado para ataque, mas sao perdidos ap  ̃ os o uso. Comec ̧a o  ́
+//jogo com 3 magias magic missile, 1 magia  ́ fireball e 1 magia teleport.
