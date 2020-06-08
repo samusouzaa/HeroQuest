5c30ee0ecd29049cb603ccdba5167b1b1b636373
lenin <lenin@lenin-IdeaPad-L340-15IRH-Gaming>
2020-06-08 01:17:04 -0300

    Criação da classe heroi e inimigo que serão filhas de gameObject

---
+++ Game/src/personagens/Heroi.java
@@ -0,0 +1,12 @@
+package personagens;
+
+public class Heroi extends GameObject {
+	
+	public Heroi(int hp, int ip, int atq, int dfs) {
+		super(hp, ip, atq, dfs);
+	}
+	
+	protected void Andar() {
+		//Ainda vamos desenvolver
+	}
+}
---
+++ Game/src/personagens/Inimigo.java
@@ -0,0 +1,11 @@
+package personagens;
+
+public class Inimigo extends GameObject {
+	public Inimigo(int hp, int ip, int atq, int dfs) {
+		super(hp, ip, atq, dfs);
+	}
+	
+	protected void Andar() {
+		//Ainda vamos desenvolver
+	}
+}
