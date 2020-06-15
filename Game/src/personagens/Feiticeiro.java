15dd419765dcbed626429baa4276040f49cb5a51
che <tatastein88@gmail.com>
2020-06-15 13:20:57 -0300

    classe feiticeiro

---
+++ Game/src/personagens/Feiticeiro.java
@@ -0,0 +1,15 @@
+package personagens;
+
+public class Feiticeiro extends Heroi {
+	private static final int ATAQUE = 1;
+	private static final int DEFESA = 2;
+	private static final int HP = 4;
+	private static final int IP = 6;
+	
+	public Feiticeiro() {
+		super(HP, IP, ATAQUE, DEFESA);
+	}
+	
+
+
+}
