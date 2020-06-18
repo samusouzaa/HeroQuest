e53e3929fbc29f93f31d2c3c64593a6b427d8ce9
samue <samue@LAPTOP-TJGE2B17>
2020-06-17 23:55:21 -0300

    Testando

---
+++ MC322HeroQuest-master/Game/src/personagens/EsqueletoMago.java
@@ -0,0 +1,24 @@
+package personagens;
+
+import java.util.Random;
+
+public class EsqueletoMago extends Inimigo{
+
+	private int posicao_atual;
+	
+	private static final int ATAQUE = 1;
+	private static final int DEFESA = 2;
+	private static final int HP = 4;
+	private static final int IP = 6;
+	
+	public EsqueletoMago() {
+		super(HP, IP, ATAQUE, DEFESA);
+	}
+	
+	public void mudarPosicao() {
+		int proxima_posicao = new Random().nextInt(8) + 1;
+		this.posicao_atual = proxima_posicao; 
+		/*terminar implementação*/
+	}
+	
+}
