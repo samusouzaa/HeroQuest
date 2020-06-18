d8a3c39d271778404a6d4ba67a6f9e882f442e9a
Pedro Andrade <pedroandrade_jp@hotmail.com>
2020-06-17 23:32:16 -0300

    Criação das classes Anão e Bárbaro de mesma forma

---
+++ Game/src/personagens/Anao.java
@@ -0,0 +1,14 @@
+package personagens;
+
+public class Anao extends Heroi {
+	private static final int ATAQUE = 2;
+	private static final int DEFESA = 2;
+	private static final int HP = 7;
+	private static final int IP = 3;
+	
+	public Anao(int x, int y) {
+		super(x, y,HP, IP, ATAQUE, DEFESA);
+		//this.equipar(true, arma);
+	}
+
+}
---
+++ Game/src/personagens/Barbaro.java
@@ -0,0 +1,15 @@
+package personagens;
+
+public class Barbaro extends Heroi {
+	private static final int ATAQUE = 3;
+	private static final int DEFESA = 2;
+	private static final int HP = 8;
+	private static final int IP = 2;
+	
+	public Barbaro(int x, int y) {
+		super(x, y, HP, IP, ATAQUE, DEFESA);
+		//this.equipar(true, arma);
+		//this.equipar(false, arma);
+	}
+
+}
