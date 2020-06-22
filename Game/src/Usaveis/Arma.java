4f6febe3bcc04463bf270aeb3a83f1ddcde52b58
samue <samue@LAPTOP-TJGE2B17>
2020-06-22 17:22:45 -0300

    Arrumando o construtor de Item

--- Game/src/Usaveis/Arma.java
+++ Game/src/Usaveis/Arma.java
@@ -5,7 +5,8 @@ public class Arma extends Item{
 	private int bonusDados;
 	private boolean ehdestruida;
 	
-	public Arma(int alcance, int bonus, boolean ehdestruida) {
+	public Arma(int alcance, int bonus, boolean ehdestruida, int x, int y, String chave) {
+		super(x,y, chave);
 		this.alcance = alcance;
 		this.bonusDados = bonus;
 		this.ehdestruida = ehdestruida;
