9ae89cc6b724d64ac23f3658cbf350b7a6c902ff
Pedro Andrade <pedroandrade_jp@hotmail.com>
2020-06-24 15:06:46 -0300

    Alterei o construtor para public pq não estava

--- Game/src/Usaveis/Item.java
+++ Game/src/Usaveis/Item.java
@@ -4,7 +4,7 @@ import elementosbasicos.Objeto;
 
 public abstract class Item extends Objeto{
 
-	Item(int x, int y, String chave) {
+	public Item(int x, int y, String chave) {
 		super(x, y);
 	}
 	
