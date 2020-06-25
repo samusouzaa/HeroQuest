6c9056a1a273d5755729511724ef0c9a78ea1d3d
samue <samue@LAPTOP-TJGE2B17>
2020-06-24 21:30:02 -0300

    Construtor Magia atualizado

--- Game/src/Usaveis/Magia.java
+++ Game/src/Usaveis/Magia.java
@@ -3,6 +3,10 @@ import elementosbasicos.GameObject;
 
 public abstract class Magia extends Item{
 	
+	Magia (String nome_magia){
+		super(nome_magia);
+	}
+	
 	public abstract void Usar(GameObject gameobject);
 	
 }
