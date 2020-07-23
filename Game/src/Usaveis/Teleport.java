bc308131fce80c9f4abd5028425f8e034d32753f
Pedro Andrade <pedroandrade_jp@hotmail.com>
2020-07-23 19:00:21 -0300

    Função teleporte pronto

--- Game/src/Usaveis/Teleport.java
+++ Game/src/Usaveis/Teleport.java
@@ -1,40 +1,111 @@
 package Usaveis;
 
 import java.util.Scanner;
+
+import Externos.Dados;
+import Externos.Direcao;
+import Externos.TipoDado;
 import elementosbasicos.Mapa;
 import elementosbasicos.GameObject;
 
-public class Teleport extends Magia{
-	
+public class Teleport extends Magia {
 
 	private static final String nome = "Teleport";
-	
+
 	public Teleport() {
 		super(nome);
 		// TODO Auto-generated constructor stub
 	}
-	
-	
+
 	public void Usar(GameObject gameobject, Mapa mapa) {
-		System.out.println("Digite as coordenadas da posição para qual se teletransportará");
+		// Jogar dados
 		Scanner keyboard = new Scanner(System.in);
-		while(true) {
-			int x = keyboard.nextInt();
-			int y = keyboard.nextInt(); 
+		Direcao direcao;
+		boolean conferido = false;
+		boolean verificado = false;
+		int xi = gameobject.getX();
+		int yi = gameobject.getY();
+		mapa.removeObjeto(gameobject);
+
+		while (!conferido || !verificado) {
+			gameobject.atualizaCoordinate(xi, yi);
+			Mapa copia = mapa.getCopia();
+			while (true) {
+				copia.addObjeto(gameobject);
+
+				System.out.println("Digite a próxima direção e [C] para confirmar a posicao");
+				String command = keyboard.nextLine();
+
+				if (command.compareTo("w") == 0)
+					direcao = Direcao.UP;
+				else if (command.compareTo("a") == 0)
+					direcao = Direcao.LEFT;
+				else if (command.compareTo("d") == 0)
+					direcao = Direcao.RIGHT;
+				else if (command.compareTo("s") == 0)
+					direcao = Direcao.DOWN;
+				else {
+					conferido = true;
+					break;
+				}
+
+				gameobject.Mover(direcao, copia);
+
+				copia.printMap();
+
+				conferido = false;
+			}
+
+			verificado = Teletransportavel(mapa, gameobject, xi, yi);
+			if(!verificado) 
+				System.out.println("Voce nao pode se teleportar para ca");
 			
-			if (Teletransportavel()) {
-				gameobject.mudarPosicao(x, y);
-				break;
-			}
-			else {
-				System.out.println("Coordenadas inválidas. Redigite-as");
-			}
+
 		}
-		
-	}
-	
-	private boolean Teletransportavel() {
-		return true;
+
+		mapa.addObjeto(gameobject);
+		mapa.printMap();
+
 	}
 
+	private boolean Teletransportavel(Mapa mapa, GameObject gameobject, int xi, int yi) {
+		int x = gameobject.getX();
+		int y = gameobject.getY();
+		if (mapa.getObjetoMapa(x, y) != null) {
+			return false;
+		} else {
+			for (int i = xi; i < mapa.getAltura(); i++) {
+				if (mapa.getObjetoMapa(i, yi) != null)
+					break;
+				if (i == x && y == yi) {
+					return true;
+				}
+			}
+			for (int i = xi; i > 0; i--) {
+				if (mapa.getObjetoMapa(i, yi) != null)
+					break;
+				if (i == x && y == yi) {
+					return true;
+				}
+			}
+
+			for (int i = yi; i < mapa.getLargura(); i++) {
+				if (mapa.getObjetoMapa(xi, i) != null)
+					break;
+				if (i == yi && x == xi) {
+					return true;
+				}
+			}
+
+			for (int i = yi; i > 0; i--) {
+				if (mapa.getObjetoMapa(xi, i) != null)
+					break;
+				if (i == yi && x == xi) {
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
 }
