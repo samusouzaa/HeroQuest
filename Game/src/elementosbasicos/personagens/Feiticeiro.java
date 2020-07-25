8d5d1499c2756a4c6ed0969eee7cf00b2bd64e2f
Pedro Andrade <pedroandrade_jp@hotmail.com>
2020-07-25 00:34:22 -0300

    Ajeitando uma troca do icone do feiticeiro

--- Game/src/elementosbasicos/personagens/Feiticeiro.java
+++ Game/src/elementosbasicos/personagens/Feiticeiro.java
@@ -5,7 +5,7 @@ public class Feiticeiro extends Heroi {
 	private static final int DEFESA = 2;
 	private static final int HP = 4;
 	private static final int IP = 6;
-	private static final String ICON = "EL";
+	private static final String ICON = "FE";
 	
 	public Feiticeiro(int x, int y) {
 		super(x, y, HP, IP, ATAQUE, DEFESA, ICON);
