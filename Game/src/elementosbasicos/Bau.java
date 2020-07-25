5420f2be5a76a8b7cc649c69205c779ff0dff43b
Pedro Andrade <pedroandrade_jp@hotmail.com>
2020-07-25 20:00:17 -0300

    Adicionei a classe bau

--- Game/src/elementosbasicos/Bau.java
+++ Game/src/elementosbasicos/Bau.java
@@ -1,6 +1,7 @@
 package elementosbasicos;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import Usaveis.Item;
 
@@ -18,6 +19,22 @@ public class Bau extends Objeto {
 	public void addItem(Item item) {
 		bau.add(item);
 	}
+	
+//	public void abrirBau() {
+//		System.out.println("Itens do bau");
+//		for(int i = 0; i < bau.size(); i++) {
+//			System.out.println("Item " + (i+1) + " : " + bau.get(i).getChave());
+//		}
+//		System.out.println("Quais itens voce quer pegar [Digite 0 para sair do bau]");
+//	
+//		Scanner keyboard = new Scanner(System.in);
+//		int command = keyboard.nextInt();
+//		
+//		while(command != 0) {
+//			
+//		}
+//		
+//	}
 	
 	
 }
